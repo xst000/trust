2dafb3c69e148f29d8506c279f844c295f75a338
xst000 <xsteezz0@gmail.com>
2024-11-27 23:19:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+import subprocess
+import time
+from threading import Thread
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def run():
-    return {"message":"success"}
+    return {"message": "success"}
+
+def run_flask():
+    app.run(host="127.0.0.1", port=8080)
+
+def run_socat():
+    subprocess.run(['socat', 'TCP-LISTEN:80,fork', 'TCP:127.0.0.1:8080'])
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080)
+    flask_thread = Thread(target=run_flask)
+    flask_thread.start()
+    time.sleep(2)
+    run_socat()
